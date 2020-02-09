005a0ce1bd4f89a274a489ebf0b398cc7eb2c257
RobinLinus <robinlinus@users.noreply.github.com>
2020-02-09 18:04:42 +0100

    Cleanup

--- src/README.md
+++ src/README.md
@@ -4,7 +4,7 @@ Helper methods for native ArrayBuffers
 ## Usage 
 
 ```
-import 'buffer' from 'https://coins.github.io/buffer-js/src/buffer.js'
+import 'buffer' from 'https://coins.github.io/buffer-js/0.0.1/buffer.js'
 
 // Do something...
 buffer
--- src/types.js
+++ src/types.js
@@ -121,3 +121,80 @@ class VarInt extends Number {
 }
 
 
+
+
+
+
+
+// class Writer {
+
+//     constructor(buffer, mode) {
+//         this._buffer = buffer;
+//         this._cursor = 0;
+//         this.mode = mode;
+//     }
+
+//     writeByte(byte) {
+//         if (this._cursor >= this._buffer.byteLength)
+//             throw new Error('Out of bounds write');
+//         this._buffer[this._cursor] = byte;
+//         this._cursor += 1;
+//     }
+
+//     writeBytes(bytes) {
+//         bytes = new Uint8Array(bytes);
+//         for (let i = 0; i < bytes.length; i++) {
+//             this.writeByte(bytes[i])
+//         }
+//     }
+
+//     result() {
+//         return this._buffer;
+//     }
+// }
+
+// class HexWriter extends Writer {
+
+//     constructor(buffer, mode) {
+//         super(buffer, mode);
+//         this._hex = '';
+//     }
+
+//     writeByte(byte) {
+//         this._hex += byteToHex(byte);
+//     }
+
+//     result() {
+//         return this._hex;
+//     }
+// }
+
+// class Reader {
+
+//     constructor(buffer) {
+//         this._buffer = buffer._buffer || buffer;
+//         this.meta = {};
+//     }
+
+//     readBytes(n) {
+//         if (n > this._buffer.length)
+//             throw new Error('Out of bounds read');
+//         const res = this._buffer.slice(0, n)
+//         this._buffer = this._buffer.slice(n);
+//         return res
+//     }
+
+//     isEmpty() {
+//         return this._buffer.length == 0;
+//     }
+
+//     bytesRemaining() {
+//         return this._buffer.length;
+//     }
+// }
+
+// class HexReader extends Reader {
+//     constructor(hex) {
+//         super(Buffer.fromHex(hex))
+//     }
+// }
